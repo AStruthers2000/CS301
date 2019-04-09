d25150dc20bb1131f3c4183f5e3330952d28e661
Andrew Struthers <StruthersA@cwu.edu>
2019-04-09 08:50:38 -0700

    Project2 Started

---
+++ NetbeansWorkspace/Project 2A given code/Employee.java
@@ -0,0 +1,43 @@
+/*
+ * Collins Employee interface, edited
+ * Project 2 A
+ */
+package project2a;
+
+/**
+ *
+ * @author lulofse
+ * Exercise 0E1 
+ * @since 03/26/19
+ * I pledge that this program represents my own program code. 
+ * I received code from and shared my code with no one.
+ */
+interface Employee {
+    
+   /**
+    *  Returns this Employee object’s name.  
+    *
+    *  @return this Employee object’s name.   
+    * 
+    */
+   String getName();
+
+
+   /**
+    *  Returns this Employee object’s gross pay.  
+    *
+    *  @return this Employee object’s gross pay.   
+    * 
+    */
+   double getGrossPay();
+
+
+   /**
+    *  Returns a String representation of this Employee object 
+    *
+    *  @return a String representation of this Employee object.
+    *
+    */
+   String toString();
+
+} // interface Employee
---
+++ NetbeansWorkspace/Project 2A given code/Project2A.java
@@ -0,0 +1,43 @@
+/*
+ * Learn jUnit
+ * Project 2 A
+ */
+package project2a;
+
+/**
+ *
+ * @author lulofse Exercise 0E1
+ * @since 03/26/19 I pledge that this program represents my own program code. I
+ * received code from and shared my code with no one.
+ */
+public class Project2A {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Project2A().run();
+    }
+
+    public void run() {
+
+        // demo JUnit test with this working project 
+        FullTimeEmployee fTobj1 = new FullTimeEmployee("Ed", 1000000.00);
+        FullTimeEmployee fTobj2 = new FullTimeEmployee("Wiley", 10.00);
+
+        System.out.println("Name= " + fTobj1.getName() + "   pay= "
+                + fTobj1.getGrossPay() + "  ");
+
+        System.out.println("Name= " + fTobj2.getName() + "   pay= "
+                + fTobj2.getGrossPay() + "  ");
+
+        System.out.println(fTobj1.toString());
+        System.out.println(fTobj1.toString());
+    }    
+}
+/*
+Name= Ed   pay= 1000000.0  
+Name= Wiley   pay= 10.0  
+Ed1000000.0 FULL TIME
+Ed1000000.0 FULL TIME
+*/
